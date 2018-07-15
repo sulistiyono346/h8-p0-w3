b6e98ddf5adc4164cb5cdd07cb5be361ff70857e
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-15 16:19:25 +0700

    Tugas Hari Rabu -Rev

--- exercises7.js
+++ exercises7.js
@@ -1,10 +1,39 @@
-console.log("1. Solve Problems array hitungJumlahKata(kalimat)");
-
 function hitungJumlahKata(kalimat) {
-  var kata = kalimat.split(" ");
-  return kata.length;
+  var temp=kalimat.length();
+  }
+  return temp;
 }
 
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
+
+function hitungJumlahKata(kalimat) {
+
+  for (var i = 0; i <= kalimat.length; i++) {
+    var temp=kalimat.split(" ")
+    return temp.length;
+  }
+}
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
+
+function hitungJumlahKata(kalimat) {
+  var kata =kalimat.split(" ");
+  var temp =0;
+  for (var i = 0; i <= kata.length; i++) {
+     temp=i
+  }
+return temp
+}
 
 // TEST CASES
 console.log(hitungJumlahKata('I have a dream')); // 4
