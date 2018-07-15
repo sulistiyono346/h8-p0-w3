98e6a2061abc66e8da9f53f1c4fd84aba8d05bf7
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-15 16:18:22 +0700

    Tugas Hari Rabu -Rev

--- exercises8.js
+++ exercises8.js
@@ -1,13 +1,32 @@
 console.log("1. Solve Problems array pasanganTerbesar(num)");
 
 function pasanganTerbesar(num) {
-  
+
   var numStr = String(num);
   var temp= 0;
   for (var i = 0; i < numStr.length; i++) {
 
     if (numStr[i] + numStr[i+1] > temp) {
-      temp = numStr[i] + numStr[i+1];
+      temp = parseInt(numStr[i] + numStr[i+1]);
+    }
+  }
+  return temp;
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
+
+function pasanganTerbesar(num) {
+  var numStr=String(num);
+  var temp=0;
+  for (var i = 0; i < numStr.length-1; i++) {
+    var joinNum=numStr[i]+numStr[i+1]
+    if (joinNum > temp) {
+      temp=joinNum
     }
   }
   return temp;
